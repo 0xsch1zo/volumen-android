12594ebfa2439a6a7af6e300664db45f470cbb6e
sentientbottleofwine <sentientbottleofwine@gmail.com>
2026-03-09 22:11:39 +0100

    mask up theme loading as best as we can

--- src/theme.ts
+++ src/theme.ts
@@ -3,22 +3,20 @@
 
     let colorPalette = await M3.getColors();
 
-    console.log(colorPalette);
-
-    if (colorPalette !== false) {
+    let failure = Object.entries(colorPalette).length == 1 && 'error' in (colorPalette as unknown as object);
+    if (colorPalette !== false && !failure) {
         for (let [color, value] of Object.entries(colorPalette)) {
-            document.documentElement.style.setProperty(`--md-sys-color-${color.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase()}`, value)
+            document.documentElement.style.setProperty(`--md-sys-color-${color.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase()}`, value as string)
         }
     } else {
-        const DEFAUlT_THEME_SEED = "#4285F4"
-        let themedRoot = document.createElement("m3e-theme")
-        themedRoot.color = DEFAUlT_THEME_SEED
-        themedRoot.motion = "expressive"
-        themedRoot.scheme = "auto"
-        let newRoot = document.createElement("div")
-        newRoot.id = "root"
-        themedRoot.appendChild(newRoot)
-        document.getElementById("root")?.replaceWith(themedRoot)
+        let {
+            themeFromSourceColor,
+            argbFromHex,
+            applyTheme,
+        } = await import("@material/material-color-utilities");
+
+        const theme = themeFromSourceColor(argbFromHex("#4285F4"));
+        const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        applyTheme(theme, { target: document.body, dark: isDark })
     }
 })()
-
