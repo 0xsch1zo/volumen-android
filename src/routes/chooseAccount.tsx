af0c2341a3534585de4b8c4ce94ff1f5c5addcbb
sentientbottleofwine <sentientbottleofwine@gmail.com>
2026-03-30 17:35:50 +0200

    properly setup logout listener

--- src/routes/chooseAccount.tsx
+++ src/routes/chooseAccount.tsx
@@ -6,6 +6,9 @@ import { Account } from "../features/chooseAccount/types.ts";
 import { M3eHeading } from "@m3e/react/heading";
 import GeometricBackground from "../components/GeometricBackground.tsx";
 import styles from "./chooseAccount.module.css"
+import { useEffect } from "react";
+import registerLogoutListener from "../utils/logoutListener.ts";
+import { useNavigate } from "react-router";
 
 type LoaderData = {
     accounts: Array<Account>
@@ -19,6 +22,14 @@ function ChooseAccountPage({
     loaderData
 }: Route.ComponentProps) {
     const { accounts } = loaderData
+    const navigate = useNavigate()
+    useEffect(() => {
+        const unlisten = registerLogoutListener(navigate)
+        return () => {
+            unlisten.then(f => f())
+        }
+    }, [navigate])
+
     return (
         <div className={styles.container}>
             <GeometricBackground />
--- src/utils/logoutListener.ts
+++ src/utils/logoutListener.ts
@@ -1,9 +1,12 @@
-import { listen } from "@tauri-apps/api/event";
+import { listen, UnlistenFn } from "@tauri-apps/api/event";
+import { NavigateFunction } from "react-router";
 
-function registerLogoutListener() {
-    listen<void>("logout", () => {
-        console.log("LOGOUT SIGNAL");
+async function registerLogoutHandler(navigate: NavigateFunction): Promise<UnlistenFn> {
+    return await listen<void>("logout", () => {
+        console.log("logout sigal")
+        if (navigate !== undefined)
+            navigate("/")
     })
 }
 
-export default registerLogoutListener
+export default registerLogoutHandler
